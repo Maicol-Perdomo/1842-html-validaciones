a2f1dbbf0da31625c3534d66edc3cad921140a72
Maicol-Perdomo <maicolperdomocesar@gmail.com>
2023-03-10 15:29:16 -0300

    Se borraron console.log inecesarios

--- js/validaciones.js
+++ js/validaciones.js
@@ -64,9 +64,6 @@ function mostrarMensajeDeError(tipoDeInput, input) {
   let mensaje = "";
   tipoDeErrores.forEach((error) => {
     if (input.validity[error]) {
-      console.log(tipoDeInput, error);
-      console.log(input.validity[error]);
-      console.log(mensajesDeError[tipoDeInput][error]);
       mensaje = mensajesDeError[tipoDeInput][error];
     }
   });
